f91ba2603802abf80fd3871f30356c07d016a017
Cezar Rosa <cecez86@gmail.com>
2021-08-12 10:43:25 -0300

    Métodos create e update para Pessoa.

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -2,6 +2,17 @@ const database = require('../models');
 
 class PessoaController
 {
+    static async create(req, res)
+    {
+        const dadosNovaPessoa = req.body;
+        try {
+            const novaPessoa = await database.Pessoas.create(dadosNovaPessoa);
+            res.status(200).json(novaPessoa);
+        } catch (erro) {
+            res.status(500).json(erro.message);
+        }
+    }
+
     static async index(req, res)
     {
         try {
@@ -18,6 +29,23 @@ class PessoaController
         try {
             const pessoa = await database.Pessoas.findByPk(Number(id));
             res.status(200).json(pessoa);
+        } catch (erro) {
+            res.status(500).json(erro.message);
+        }
+    }
+
+    static async update(req, res) 
+    {
+        const { id } = req.params;
+        const dadosParaAtualizar = req.body;
+
+        try {
+            const pessoa = await database.Pessoas.findByPk(id);
+            pessoa.set(dadosParaAtualizar);
+            const pessoaAtualizada = await pessoa.save();
+
+            res.status(200).json(pessoaAtualizada);
+
         } catch (erro) {
             res.status(500).json(erro.message);
         }
--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -5,5 +5,8 @@ const router = Router();
 
 router.get('/pessoas', PessoaController.index);
 router.get('/pessoas/:id', PessoaController.show);
+router.post('/pessoas', PessoaController.create);
+router.put('/pessoas/:id', PessoaController.update);
+
 
 module.exports = router;
